493f690389affd3b43a1385c307feef8e2cea017
David Declerck <david.declerck@ocamlpro.com>
2025-04-22 16:08:58 +0200

    Merge SVN 5285

--- cobc/tree.c
+++ cobc/tree.c
@@ -3890,14 +3890,13 @@ repeat:
 				category |= PIC_NUMERIC_EDITED;
 				if (c_count == 0) {
 					digits += n - 1;
-					c_count = n - 1;
 				} else {
 					digits += n;
-					c_count += n;
 				}
 				if (v_count) {		/* Handle:  $$$.$$ */
 					scale += n;
 				}
+				c_count += n;
 				break;
 			}
 
